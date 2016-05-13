62b0a18ee68b6110cc03607ad637f3bf9d290366
snugghash <snugghash@gmail.com>
2016-05-13 19:44:41 +0530

    Added simple behaviourTest.m file.

--- src/getGreedyAction.m
+++ src/getGreedyAction.m
@@ -1,6 +1,6 @@
 function maxAction = getGreedyAction(obsState, theta)
 maxValue = -Inf;
-maxAction = 1;
+maxAction = 3; %TODO Made the default greedy action 3, so we know why it's spinning.
 for j=1:3
     tmp = actionValueApprox(theta, obsState, j);
     if tmp>=maxValue
