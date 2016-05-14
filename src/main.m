51f76d0adbb9b14dc44c4018ca700cb9656c1f1f
Vishruit <princepaulite@gmail.com>
2016-05-14 22:39:45 +0530

    Backing up my copy of main.m

--- src/main.m
+++ src/main.m
@@ -5,8 +5,9 @@ if dbg
         clearvars -except dbg wipeOut %TODO remove if coming from previous theta. Add to options, like debug.
     end
 end
+% init;
 
-global  sideWings vec moveSlow gridSize axPosition axReward positionPlot quiverPlot quiverSidePlot eyes blobsEaten;
+global  sideWings theta vec moveSlow gridSize axPosition axReward positionPlot quiverPlot quiverSidePlot eyes blobsEaten;
 
 % Setting up axes for the plots and hold 'on'
 axPosition = subplot(3,3,[1 2 3 4 5 6]);
@@ -44,7 +45,7 @@ steps = 0;
 if dbg == 1 
     display('In main: before while');
 end
-j=0;counter =0;
+j=0;counter =1;
 while steps<=max_steps
     if dbg == 1 
         display(['Steps(Age): ' num2str(steps)]);
@@ -52,6 +53,7 @@ while steps<=max_steps
         display(obsEnv);
     end
     avgReward = 0;
+    R(counter) = 0;
     for i=1:frame
         obsEnv = observableEnv(EnvState, EnvState(1,[1 2]), EnvState(1,[3 4]));
         action = squareAgent(obsEnv, reward);
@@ -63,7 +65,7 @@ while steps<=max_steps
             display(reward);
         end
         counter = counter + 1;
-        R(counter) = reward;
+        R(counter) = R(counter-1) + reward;
     end
     avgReward = avgReward/frame;
     % To display all the previous points in the plot.
