8575725ac363def4081c554d517530fc459c3768
Vishruit <princepaulite@gmail.com>
2016-05-15 15:46:07 +0530

    CHanged variable name

--- src/getGreedyAction.m
+++ src/getGreedyAction.m
@@ -1,11 +1,11 @@
 function maxAction = getGreedyAction(obsState, theta)
 maxValue = -Inf;
 maxAction = 3; %TODO Made the default greedy action 3, so we know why it's spinning.
-for j=1:3
-    tmp = actionValueApprox(theta, obsState, j);
+for j_action=1:3
+    tmp = actionValueApprox(theta, obsState, j_action);
     if tmp>=maxValue % TODO: Should it be in absolute terms?
         maxValue = tmp;
-        maxAction = j;
+        maxAction = j_action;
     end
 end
 end
