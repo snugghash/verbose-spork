639531cbc588e23be97cb4c075b56c09f06869e1
Vishruit <princepaulite@gmail.com>
2016-05-15 18:39:44 +0530

    Updated definition to actionToEye to have a overlap of sensors

--- src/actionToEye.m
+++ src/actionToEye.m
@@ -1,15 +1,14 @@
 function [ i] = actionToEye( action )
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-global eyes angleOfVision turnRate;
+global eyes angleOfVision turnRate sensorOverlap;
 if(action == 1)
-    i = round(eyes/2);
+    i = [(round(eyes/2)-floor(sensorOverlap/2)):(round(eyes/2)+floor(sensorOverlap/2))];
 elseif(action == 3)
     %i = (angleOfVision-turnRate)/angleOfVision*eyes;
-    i = [round(eyes/2)+1:eyes];
+    i = [(eyes-sensorOverlap+1):eyes];
 elseif(action == 2)
     %i = eyes-(angleOfVision-turnRate)/angleOfVision*eyes;
-    i = [1:round(eyes/2)-1];
+    i = [1:sensorOverlap];
 end
 end
-
